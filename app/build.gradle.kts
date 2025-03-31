626ed0758746d974b6ce5eb456e26349f73efb51
cwsong0117 <cwsong0833@gmail.com>
2025-03-31 15:21:58 +0800

    Merge remote-tracking branch 'origin/master'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.navigation:navigation-compose:2.8.9") // Use latest version
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
