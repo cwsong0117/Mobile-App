fc768b7063cbd2a702fb869825d628c72bf07ca5
cwsong0117 <cwsong0833@gmail.com>
2025-03-31 16:48:08 +0800

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/hermen/ass1/MainActivity.kt
+++ app/src/main/java/com/hermen/ass1/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ass1Theme {
-                MainScreen()
+                MeetingRoomApply()
             }
         }
     }
