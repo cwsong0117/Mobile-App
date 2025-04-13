c070d41c5cdb2bcac0778d05a62f152cc4d03efe
leekeezhan <leekeezhan@gmail.com>
2025-04-14 04:50:26 +0800

    Merge branch 'qizhan'

--- app/src/main/java/com/hermen/ass1/LoginScreen.kt
+++ app/src/main/java/com/hermen/ass1/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.hermen.ass1
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -17,9 +18,33 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.material3.Text
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.material3.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.TextField
+
+
+
 
 @Composable
 fun LoginScreen(navController: NavController) {
+    val username = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -33,6 +58,102 @@ fun LoginScreen(navController: NavController) {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
+            // 文字部分
+            Spacer(modifier = Modifier.height(100.dp)) // 调整高度，推开顶部
+
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(start = 30.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Login",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                }
+            }
+
+            // 在文字和图标之间增加间隔
+            Spacer(modifier = Modifier.height(50.dp))
+
+            // 图标部分
+            Box(
+                modifier = Modifier
+                    .size(200 .dp) // 方形 Box
+                    .clip(RoundedCornerShape(12.dp))
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.app_logo), // 记得添加资源
+                    contentDescription = "App Logo",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.fillMaxSize()
+                )
+
+            }
+            Text(
+                text = stringResource(R.string.app_name),
+                color = Color.Black,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Column(
+                modifier = Modifier
+                    .padding(start = 30.dp, end = 30.dp)
+            ) {
+                Text(
+                    text = "Username",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 4.dp)
+                )
+                TextField(
+                    value = username.value,
+                    onValueChange = { username.value = it },
+                    placeholder = { Text("ex: hello") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(50.dp)),
+                    singleLine = true
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(
+                    text = "Password",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 4.dp)
+                )
+                TextField(
+                    value = password.value,
+                    onValueChange = { password.value = it },
+                    placeholder = { Text("ex: ******") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(50.dp)),
+                    singleLine = true
+                )
+            }
+            Text(text = "Username: ${username.value}")
+            Text(text = "Password: ${password.value}")
+
+            Spacer(modifier = Modifier.height(50.dp))
+
             Text(text = "This is the Login Screen")
             Text(
                 text = "← Back",
@@ -41,7 +162,6 @@ fun LoginScreen(navController: NavController) {
                     .clickable { navController.popBackStack() },
                 color = Color.Blue
             )
-
         }
     }
 }
--- app/src/main/java/com/hermen/ass1/SignUpScreen.kt
+++ app/src/main/java/com/hermen/ass1/SignUpScreen.kt
@@ -1,7 +1,10 @@
 package com.hermen.ass1
 
+import android.app.DatePickerDialog
+import android.widget.DatePicker
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,12 +16,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -26,10 +33,32 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import java.util.Calendar
 
 
 @Composable
 fun SignupScreen(navController: NavController) {
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH)
+    val day = calendar.get(Calendar.DAY_OF_MONTH)
+    val fullname = remember { mutableStateOf("") }
+    val birthday = remember { mutableStateOf("") }
+    val username = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+    val confirmpassword = remember { mutableStateOf("") }
+
+    val datePickerDialog = DatePickerDialog(
+        context,
+        { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDayOfMonth: Int ->
+            val formattedMonth = String.format("%02d", selectedMonth + 1)
+            val formattedDay = String.format("%02d", selectedDayOfMonth)
+            birthday.value = "$formattedMonth/$formattedDay/$selectedYear"
+        }, year, month, day
+    )
+
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -44,7 +73,7 @@ fun SignupScreen(navController: NavController) {
             verticalArrangement = Arrangement.Top
         ) {
             // 文字部分
-            Spacer(modifier = Modifier.height(100.dp)) // 调整高度，推开顶部
+            Spacer(modifier = Modifier.height(50.dp)) // 调整高度，推开顶部
 
             Box(
                 modifier = Modifier.fillMaxWidth(),
@@ -53,18 +82,12 @@ fun SignupScreen(navController: NavController) {
                 Column(
                     modifier = Modifier
                         .align(Alignment.CenterStart)
-                        .padding(start = 60.dp),
+                        .padding(start = 230.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(
-                        text = "GOOD",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.Black
-                    )
-                    Text(
-                        text = "  DAY!",
-                        fontSize = 24.sp,
+                        text = "Signup",
+                        fontSize = 30.sp,
                         fontWeight = FontWeight.Bold,
                         color = Color.Black
                     )
@@ -74,27 +97,112 @@ fun SignupScreen(navController: NavController) {
             // 在文字和图标之间增加间隔
             Spacer(modifier = Modifier.height(50.dp))
 
-            // 图标部分
-            Box(
+            Column(
                 modifier = Modifier
-                    .size(200 .dp) // 方形 Box
-                    .clip(RoundedCornerShape(12.dp))
+                    .padding(start = 30.dp, end = 30.dp)
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.app_logo), // 记得添加资源
-                    contentDescription = "App Logo",
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier.fillMaxSize()
+                Text(
+                    text = "Full Name as per IC",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 4.dp)
                 )
-
+                TextField(
+                    value = fullname.value,
+                    onValueChange = { fullname.value = it },
+                    placeholder = { Text("ex: LEE KEE ZHAN") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(50.dp)),
+                    singleLine = true
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(
+                    text = "Date od Birth",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 4.dp)
+                )
+                TextField(
+                    value = birthday.value,
+                    onValueChange = { birthday.value = it },
+                    placeholder = { Text("MM/DD/YYYY") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(50.dp))
+                        .clickable {
+                            datePickerDialog.show()
+                        },
+                    enabled = false, // 让TextField不可编辑，必须通过选择器输入
+                    singleLine = true
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(
+                    text = "Username",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 4.dp)
+                )
+                TextField(
+                    value = username.value,
+                    onValueChange = { username.value = it },
+                    placeholder = { Text("ex: Ah Lee") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(50.dp)),
+                    singleLine = true
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(
+                    text = "Password",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 4.dp)
+                )
+                TextField(
+                    value = password.value,
+                    onValueChange = { password.value = it },
+                    placeholder = { Text("ex: ******") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(50.dp)),
+                    singleLine = true
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(
+                    text = "Confirm Password",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 4.dp)
+                )
+                TextField(
+                    value = confirmpassword.value,
+                    onValueChange = { confirmpassword.value = it },
+                    placeholder = { Text("ex: ******") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(50.dp)),
+                    singleLine = true
+                )
+                Spacer(modifier = Modifier.height(30.dp))
             }
-            Text(
-                text = stringResource(R.string.app_name),
-                color = Color.Black,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
+            Text(text = "Username: ${username.value}")
+            Text(text = "Password: ${password.value}")
 
             // 在图标和按钮之间增加间距
             Spacer(modifier = Modifier.height(50.dp))
